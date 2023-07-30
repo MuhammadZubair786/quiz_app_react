09253047a2d6e79063df2ed5bd2f231ccf5a3988
MuhammadZubair786 <mzhassan444@gmail.com>
2023-07-30 05:09:59 -0700

    add files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
 import logo from './logo.svg';
 import './App.css';
+import Questions from './Questions';
+import { useState } from 'react';
 
 function App() {
+
+  let [currentindex, setcurrentindex] = useState(0)
+
+  let [score, setscore] = useState(0)
+
+  let [showres, setshowres] = useState(false)
+
+  let obj = [
+    {
+      "Quiz": "HTML is the standard ____language for creating Web pages",
+      "Options": [
+        "scripting",
+        "markup",
+        "programming",
+        "styling"
+      ],
+      "Correct_ans": "markup"
+    },
+    {
+      "Quiz": "Which HTML element is used to define description data?",
+      "Options": [
+        "li",
+        "Ol",
+        "dl",
+        "dd"
+      ],
+      "Correct_ans": "dd"
+    },
+    {
+      "Quiz": "Which of the following element is responsible for making the text bold in HTML?",
+      "Options": [
+        "<pre>",
+        "<a>",
+        "<b>",
+        "<br>"
+      ],
+      "Correct_ans": "<b>"
+    },
+    {
+      "Quiz": "How to create an ordered list (a list with the list items in numbers) in HTML?",
+      "Options": [
+        "<ul>",
+        "<ol>",
+        "<dl>",
+        "<li>"
+      ],
+      "Correct_ans": "<ol>"
+    },
+
+  ]
+
+  const callback = (data) => {
+    console.log(data)
+    if (data == obj[currentindex]["Correct_ans"]) {
+      setscore(++score)
+
+    }
+    if (currentindex >= obj.length - 1) {
+      setshowres(true)
+
+    }
+    else {
+      setcurrentindex(++currentindex)
+
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      {/* {
+      obj.map((v,i)=>{
+        return(
+          <h1>Question No : {i+1}</h1>
+        )
+      })
+    } */}
+
+      {
+        showres ?
+          <h1>Score : {score}</h1>
+          :
+          <Questions
+            question={obj[currentindex]["Quiz"]}
+            options={obj[currentindex]["Options"]}
+            i={currentindex}
+            callback={callback} //function
+          />
+      }
+
+
+    </>
+
   );
 }
 
